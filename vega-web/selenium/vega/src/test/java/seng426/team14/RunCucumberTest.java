a9eea78b3729b7e8c03a7b0475ef6a6560baccc1
chemxy <chemxy110@gmail.com>
2022-06-21 15:37:44 -0700

    init selenium + cucumber

---
+++ vega-web/selenium/vega/src/test/java/seng426/team14/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package seng426.team14;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("seng426/team14")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class RunCucumberTest {
+}
