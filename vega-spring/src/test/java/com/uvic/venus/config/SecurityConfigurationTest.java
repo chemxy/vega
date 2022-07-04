c6de688a3f8a81391d0e8d6c5e51d778bb615053
matth <matthewmclean@uvic.ca>
2022-07-04 00:35:06 -0700

    Finalized tests for configureGlobal and passwordEncoder, remaining methods require integration tests

--- vega-spring/src/test/java/com/uvic/venus/config/SecurityConfigurationTest.java
+++ vega-spring/src/test/java/com/uvic/venus/config/SecurityConfigurationTest.java
@@ -1,13 +1,6 @@
 package com.uvic.venus.config;
 
-import com.uvic.venus.auth.JwtTokenFilter;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.JdbcUserDetailsManagerConfigurer;
@@ -20,33 +13,26 @@ import static org.mockito.Mockito.*;
 class SecurityConfigurationTest {
 
     AuthenticationManagerBuilder auth = mock(AuthenticationManagerBuilder.class);
-    DataSource dataSource = mock(DataSource.class);
-
     SecurityConfiguration securityConfiguration = new SecurityConfiguration();
 
     @Test
+    //Unit test for SecurityConfiguration.passwordEncoder
+    void passwordEncoder() {
+        PasswordEncoder encoder = securityConfiguration.passwordEncoder();
+        assert(encoder != null);
+    }
+    @Test
+    //Unit test for SecurityConfiguration.configureGlobal
     void configureGlobal() {
+        JdbcUserDetailsManagerConfigurer jdbc = mock(JdbcUserDetailsManagerConfigurer.class);
         try {
-            JdbcUserDetailsManagerConfigurer jdbc = mock(JdbcUserDetailsManagerConfigurer.class);
             when(auth.jdbcAuthentication()).thenReturn(jdbc);
             securityConfiguration.configureGlobal(auth);
-            verify(auth.jdbcAuthentication());
+            verify(auth).jdbcAuthentication();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    @Test
-    void configure() {
-    }
-
-    @Test
-    void authenticationManagerBean() {
-    }
-
-    @Test
-    void passwordEncoder() {
-        PasswordEncoder encoder = securityConfiguration.passwordEncoder();
-        assert(encoder != null);
-    }
+    //The remaining methods in SecurityConfiguration.java require integration testing rather than unit testing
 }
