63197a2b6587648ee079af1dbe7f071cd290ad2a
chemxy <chemxy110@gmail.com>
2022-07-04 17:34:32 -0700

    fix resource fetching issue

--- vega-spring/src/main/java/com/uvic/venus/VenusApplication.java
+++ vega-spring/src/main/java/com/uvic/venus/VenusApplication.java
@@ -1,20 +1,31 @@
 package com.uvic.venus;
 
 import com.uvic.venus.storage.StorageProperties;
+import com.uvic.venus.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
 @EnableWebSecurity
 @EnableConfigurationProperties(StorageProperties.class)
 @PropertySource("classpath:application-dev.properties")
-public class VenusApplication {
+public class VenusApplication implements CommandLineRunner {
+	@Resource
+	StorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VenusApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+	}
 }
