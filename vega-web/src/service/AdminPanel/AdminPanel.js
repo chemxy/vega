6c69d9cc2b0c5a6f22560eab46c4b792ba99592a
chemxy <chemxy110@gmail.com>
2022-08-02 19:05:22 -0700

    revert and refactor

--- vega-web/src/service/AdminPanel/AdminPanel.js
+++ vega-web/src/service/AdminPanel/AdminPanel.js
@@ -1,4 +1,4 @@
-import { doPostFile, doGet, doPost } from '../BaseAPI.js';
+import { doPostFile, doGet, doPost, doPostWithToken } from '../BaseAPI.js';
 import { domain } from '../../models/constants.js';
 
 export function fetchuser(token) {
@@ -11,9 +11,10 @@ export function enableAccount(username, token) {
 		"username": username,
 		"enable": "true",
 	}
-	return doPostFile("http://" + domain + ":8000/api/venus/admin/enableuser", body, token)
+	return doPostWithToken("http://" + domain + ":8000/api/venus/admin/enableuser", body, token)
 }
 
 export function changeAccountRole(username, role, token) {
-	return doGet("http://" + domain + ":8000/api/venus/admin/changerole?username=" + username + "&role=" + role, token)
+	// return doGet("http://" + domain + ":8000/api/venus/admin/changerole?username=" + username + "&role=" + role, token)
+	return doPostWithToken("http://" + domain + ":8000/api/venus/admin/changerole?username=" + username + "&role=" + role, token)
 }
--- vega-web/src/service/BaseAPI.js
+++ vega-web/src/service/BaseAPI.js
@@ -46,9 +46,25 @@ export async function doPost(url, data) {
   return await handleResponse(response);
 }
 
-export async function doPostFile(url, data, token) {
+export async function doPostWithToken(url, data, token) {
   console.log(getUserToken());
   console.log('Request data:', data);
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + token,
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  });
+
+  return await handleResponse(response);
+}
+
+export async function doPostFile(url, data, token) {
+  debugLog(getUserToken());
+  debugLog('Request data:', data);
   const response = await fetch(url, {
     method: 'POST',
     headers: {
